54274602748d05e32e101587b29de07881a5fa32
Ken Ho <kenhks014@gmail.com>
2022-03-09 11:33:38 +0800

    add Linked-List structure

---
+++ utils.py
@@ -0,0 +1,19 @@
+from typing import List
+
+from data_structures import ListNode
+
+
+def create_LinkedList(nums: List) -> ListNode:
+    prev_node = head = ListNode(nums[0])
+    for i in nums[1:]:
+        prev_node.next = ListNode(i)
+        prev_node = prev_node.next
+    return head
+
+
+def get_LinkedList_values(node: ListNode) -> List:
+    values = []
+    while node:
+        values.append(node.val)
+        node = node.next
+    return values
