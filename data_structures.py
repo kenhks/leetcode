c5d1179a310b02da7bee6f2d45a0b0567451474e
Ken Ho <kenhks014@gmail.com>
2022-03-12 02:04:19 +0800

    add data structure node

--- data_structures.py
+++ data_structures.py
@@ -2,3 +2,8 @@ class ListNode:
     def __init__(self, val=0, next=None):
         self.val = val
         self.next = next
+
+class Node:
+    def __init__(self, val=None, children=None):
+        self.val = val
+        self.children = children
