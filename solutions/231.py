ae8ce10d066566bac71ac7af6b6199f3e17da3b6
Ken Ho <kenhks014@gmail.com>
2022-03-17 09:51:14 +0800

    add second solution 231

--- solutions/231.py
+++ solutions/231.py
@@ -3,6 +3,11 @@ class Solution:
         return n > 0 and "{:b}".format(n).count("1") == 1
 
 
+class Solution2:
+    def isPowerOfTwo(self, n):
+        return n and not (n & n - 1)
+
+
 def test_1():
     assert Solution().isPowerOfTwo(2) == True
 
@@ -25,3 +30,27 @@ def test_5():
 
 def test_6():
     assert Solution().isPowerOfTwo(17) == False
+
+
+def test_7():
+    assert Solution2().isPowerOfTwo(2) == True
+
+
+def test_8():
+    assert Solution2().isPowerOfTwo(1) == True
+
+
+def test_9():
+    assert Solution2().isPowerOfTwo(8) == True
+
+
+def test_10():
+    assert Solution2().isPowerOfTwo(-8) == False
+
+
+def test_11():
+    assert Solution2().isPowerOfTwo(3) == False
+
+
+def test_12():
+    assert Solution2().isPowerOfTwo(17) == False
