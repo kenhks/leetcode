aa5799ae4b023777c59607ae97965b4b91d9f1b2
Ken Ho <kenhks014@gmail.com>
2022-07-01 18:00:00 +0800

    fix createLinkedList for empty list

--- utils.py
+++ utils.py
@@ -4,6 +4,8 @@ from data_structures import ListNode
 
 
 def create_LinkedList(nums: List) -> ListNode:
+    if not nums:
+        return None
     prev_node = head = ListNode(nums[0])
     for i in nums[1:]:
         prev_node.next = ListNode(i)
