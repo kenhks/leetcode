9f7fc5c117adb04ad14f41d0b84799cb4ec29281
Ken Sing <ken.sing@contineo.com.hk>
2022-07-12 18:40:18 +0800

    solution1: test sorted value

--- solutions/1.py
+++ solutions/1.py
@@ -17,7 +17,7 @@ class Solution:
 class Solution2:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
         for i, x in enumerate(nums):
-            for j, y in enumerate(nums[i + 1:]):
+            for j, y in enumerate(nums[i + 1 :]):
                 if x + y == target:
                     return [i, i + j + 1]
 
@@ -33,19 +33,19 @@ solutions = parametrize_solution_cls(
 
 @pytest.mark.parametrize("solution", solutions)
 def test_1(solution):
-    assert solution([2, 7, 11, 15], 9) == [0, 1]
+    assert sorted(solution([2, 7, 11, 15], 9)) == [0, 1]
 
 
 @pytest.mark.parametrize("solution", solutions)
 def test_2(solution):
-    assert solution([3, 2, 4], 6) == [1, 2]
+    assert sorted(solution([3, 2, 4], 6)) == [1, 2]
 
 
 @pytest.mark.parametrize("solution", solutions)
 def test_3(solution):
-    assert solution([3, 3], 6) == [0, 1]
+    assert sorted(solution([3, 3], 6)) == [0, 1]
 
 
 @pytest.mark.parametrize("solution", solutions)
 def test_4(solution):
-    assert solution([-3, 3, 10, 8], 5) == [0, 3]
+    assert sorted(solution([-3, 3, 10, 8], 5)) == [0, 3]
