fd347407895539e5b77656ee5f755a9704c30404
Ken Ho <kenhks014@gmail.com>
2022-07-12 13:30:00 +0800

    add solution 238

---
+++ solutions/238.py
@@ -0,0 +1,77 @@
+from typing import List
+from itertools import accumulate, product
+
+import pytest
+
+from utils import parametrize_solution_cls
+
+
+class Solution:
+    """
+    Time Complexity: O(n^2)
+    Space Complexity: O(n)
+    """
+
+    def productExceptSelf(self, nums: List[int]) -> List[int]:
+        prod = [1] * len(nums)
+        for i, x in enumerate(nums):
+            for j, y in enumerate(prod):
+                if i != j:
+                    prod[j] = y * x
+        return prod
+
+
+class Solution2:
+    """
+    Time Complexity: O(3n)
+    Space Complexity: O(3n)
+    """
+
+    def productExceptSelf(self, nums: List[int]) -> List[int]:
+        left_cum_prod = [1] + list(accumulate(nums, lambda a, b: a * b))
+        right_cum_prod = [1] + list(accumulate(nums[::-1], lambda a, b: a * b))
+        prod = []
+        for i in range(len(nums)):
+            prod.append(left_cum_prod[i] * right_cum_prod[-2 - i])
+        return prod
+
+
+class Solution3:
+    """
+    Time Complexity: O(3n)
+    Space Complexity: O(n)
+    """
+
+    def productExceptSelf(self, nums: List[int]) -> List[int]:
+        size = len(nums)
+        prod = [1] * size
+        left_product = right_product = 1
+        for i, x in enumerate(nums):
+            if i > 0:
+                prod[i] = left_product
+            left_product *= x
+        for j, y in enumerate(nums[::-1]):
+            if j > 0:
+                prod[size - j - 1] *= right_product
+            right_product *= y
+        return prod
+
+
+solutions = parametrize_solution_cls(
+    [
+        Solution,
+        Solution2,
+        Solution3,
+    ],
+    "productExceptSelf",
+)
+
+
+@pytest.mark.parametrize("solution", solutions)
+def test_1(solution):
+    assert solution([1, 2, 3, 4]) == [24, 12, 8, 6]
+
+
+@pytest.mark.parametrize("solution", solutions)
+def test_2(solution):
+    assert solution([-1, 1, 0, -3, 3]) == [0, 0, 9, 0, 0]
