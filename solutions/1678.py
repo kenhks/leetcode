f51c8f238bb4ee1edbc806af302f02264e429420
Ken Ho <kenhks014@gmail.com>
2022-08-07 20:40:00 +0800

    parametric solution 1678

--- solutions/1678.py
+++ solutions/1678.py
@@ -1,9 +1,26 @@
+import pytest
+
+from utils import parametrize_solution_cls
+
+
 class Solution:
+    """
+    Python built-in replace()
+
+    Time Complexity: O(n) = 2n
+    Space Complexity: O(n) = 3n
+    """
+
     def interpret(self, command: str) -> str:
         return command.replace("()", "o").replace("(al)", "al")
 
 
 class Solution2:
+    """
+    Time Complexity: O(n) = n
+    Space Complexity: O(n) = n
+    """
+
     def interpret(self, command: str) -> str:
         size = len(command)
         i = 0
@@ -14,36 +31,34 @@ class Solution2:
                 if command[i + 1] == ")":
                     real_cmd += "o"
                     i += 2
-                elif command[i + 1: i + 4] == "al)":
+                elif command[i + 1 : i + 4] == "al)":
                     real_cmd += "al"
                     i += 4
             else:
                 real_cmd += "G"
                 i += 1
         return real_cmd
-        
 
 
-
-def test_1():
-    assert Solution().interpret("G()(al)") == "Goal"
-
-
-def test_2():
-    assert Solution().interpret("G()()()()(al)") == "Gooooal"
-
-
-def test_3():
-    assert Solution().interpret("(al)G(al)()()G") == "alGalooG"
+solutions = parametrize_solution_cls(
+    [
+        Solution,
+        Solution2,
+    ],
+    "interpret",
+)
 
 
-def test_4():
-    assert Solution2().interpret("G()(al)") == "Goal"
+@pytest.mark.parametrize("solution", solutions)
+def test_1(solution):
+    assert solution("G()(al)") == "Goal"
 
 
-def test_5():
-    assert Solution2().interpret("G()()()()(al)") == "Gooooal"
+@pytest.mark.parametrize("solution", solutions)
+def test_2(solution):
+    assert solution("G()()()()(al)") == "Gooooal"
 
 
-def test_6():
-    assert Solution2().interpret("(al)G(al)()()G") == "alGalooG"
+@pytest.mark.parametrize("solution", solutions)
+def test_3(solution):
+    assert solution("(al)G(al)()()G") == "alGalooG"
