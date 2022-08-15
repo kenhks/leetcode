4ad3a052547e44a11345b62088df2bf1c0e8b490
Ken Ho <kenhks014@gmail.com>
2022-08-15 18:55:00 +0800

    parametric solution 1672

--- solutions/1672.py
+++ solutions/1672.py
@@ -1,21 +1,39 @@
 from typing import List
 
+import pytest
+
+from utils import parametrize_solution_cls
+
 
 class Solution:
+    """
+    Brute Force
+    Time Complexity: O(mn)
+    Space Complexity: O(1)
+    """
+
     def maximumWealth(self, accounts: List[List[int]]) -> int:
-        return max(sum(ac) for ac in accounts)
+        return max(sum(row) for row in accounts)
 
 
-def test_1():
-    accounts = [[1, 2, 3], [3, 2, 1]]
-    assert Solution().maximumWealth(accounts) == 6
+solutions = parametrize_solution_cls(
+    [
+        Solution,
+    ],
+    "maximumWealth",
+)
 
 
-def test_2():
-    accounts = [[1, 5], [7, 3], [3, 5]]
-    assert Solution().maximumWealth(accounts) == 10
+@pytest.mark.parametrize("solution", solutions)
+def test_1(solution):
+    assert solution([[1,2,3],[3,2,1]]) == 6
 
 
-def test_3():
-    accounts = [[2, 8, 7], [7, 1, 3], [1, 9, 5]]
-    assert Solution().maximumWealth(accounts) == 17
+@pytest.mark.parametrize("solution", solutions)
+def test_2(solution):
+    assert solution([[1,5],[7,3],[3,5]]) == 10
+
+
+@pytest.mark.parametrize("solution", solutions)
+def test_3(solution):
+    assert solution([[2,8,7],[7,1,3],[1,9,5]]) == 17
