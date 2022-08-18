ed63bb89b8a29c5de7dca5a1d7bebb2434dfc678
Ken Ho <kenhks014@gmail.com>
2022-08-19 01:26:03 +0800

    improve solution 125

--- solutions/125.py
+++ solutions/125.py
@@ -4,10 +4,16 @@ from utils import parametrize_solution_cls
 
 
 class Solution:
+    """
+    Check with two pointer
+    Time Complexity: O(n) = 2n
+    Space Complexity: O(n) = n
+    """
+
     def isPalindrome(self, s: str) -> bool:
         valid_chars = [i for i in s.lower() if i.isalnum()]
         left, right = 0, len(valid_chars) - 1
-        while left <= right:
+        while left < right:
             if valid_chars[left] != valid_chars[right]:
                 return False
             left += 1
@@ -15,9 +21,22 @@ class Solution:
         return True
 
 
+class Solution2:
+    """
+    Check with two pointer
+    Time Complexity: O(n) = 2n
+    Space Complexity: O(n) = 2n
+    """
+
+    def isPalindrome(self, s: str) -> bool:
+        valid_chars = [i for i in s.lower() if i.isalnum()]
+        return valid_chars == valid_chars[::-1]
+
+
 solutions = parametrize_solution_cls(
     [
         Solution,
+        Solution2,
     ],
     "isPalindrome",
 )
