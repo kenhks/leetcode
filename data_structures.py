ffb0279cafb8979ba809999a831a5bb4ed880537
Ken Ho <kenhks014@gmail.com>
2022-08-20 20:43:01 +0800

    add verbose output for ListNode and TreeNode

--- data_structures.py
+++ data_structures.py
@@ -4,7 +4,10 @@ class ListNode:
         self.next = next
 
     def __str__(self):
-        return f"{self.val, self.next = }"
+        return f"{self.val, self.next}"
+
+    def __repr__(self):
+        return f"{self.__class__.__name__}(val={self.val})"
 
 
 class Node:
@@ -18,3 +21,9 @@ class TreeNode:
         self.val = val
         self.left = left
         self.right = right
+
+    def __str__(self):
+        return f"{self.val, self.left, self.right}"
+
+    def __repr__(self):
+        return f"{self.__class__.__name__}(val={self.val})"
