8e448e62fcb83322dcbdfe1ecf1921426d5d3948
Ken Ho <kenhks014@gmail.com>
2022-08-21 17:46:21 +0800

    parametric solution 704

--- solutions/704.py
+++ solutions/704.py
@@ -1,24 +1,44 @@
 from typing import List
 
+import pytest
 
-def search(nums: List[int], target: int) -> int:
-    index = -1
-    left, right = 0, len(nums) - 1
-    while left <= right:
-        mid = (left + right) // 2
-        if nums[mid] == target:
-            index = mid
-            break
-        elif nums[mid] > target:
-            right = mid - 1
-        elif nums[mid] < target:
-            left = mid + 1
-    return index
+from utils import parametrize_solution_cls
 
 
-def test_1():
-    assert search([-1, 0, 3, 5, 9, 12], 9) == 4
+class Solution:
+    """
+    Time Complexity: O(log(2, n))
+    Space Complexity: O(1)
+    """
+
+    def search(nums: List[int], target: int) -> int:
+        index = -1
+        left, right = 0, len(nums) - 1
+        while left <= right:
+            mid = (left + right) // 2
+            if nums[mid] == target:
+                index = mid
+                break
+            elif nums[mid] > target:
+                right = mid - 1
+            elif nums[mid] < target:
+                left = mid + 1
+        return index
 
 
-def test_2():
-    assert search([-1, 0, 3, 5, 9, 12], 2) == -1
+solutions = parametrize_solution_cls(
+    [
+        Solution,
+    ],
+    "search",
+)
+
+
+@pytest.mark.parametrize("solution", solutions)
+def test_1(solution):
+    assert solution([-1, 0, 3, 5, 9, 12], 9) == 4
+
+
+@pytest.mark.parametrize("solution", solutions)
+def test_2(solution):
+    assert solution([-1, 0, 3, 5, 9, 12], 2) == -1
