0222261f92b0128ed7966c0aff83a102fe9ddf70
Ken Ho <kenhks014@gmail.com>
2022-08-25 13:49:10 +0800

    parametric solution 35

--- solutions/35.py
+++ solutions/35.py
@@ -1,6 +1,17 @@
 from typing import List
 
+import pytest
+
+from utils import parametrize_solution_cls
+
+
 class Solution:
+    """
+    Binary search
+    Time Complexity: O(log(2, n))
+    Space Complexity: O(1)
+    """
+
     def searchInsert(self, nums: List[int], target: int) -> int:
         if target < nums[0]:
             return 0
@@ -19,13 +30,24 @@ class Solution:
         return left
 
 
-def test_1():
-    assert Solution().searchInsert([1, 3, 5, 6], 5) == 2
+solutions = parametrize_solution_cls(
+    [
+        Solution,
+    ],
+    "searchInsert",
+)
 
 
-def test_2():
-    assert Solution().searchInsert([1, 3, 5, 6], 2) == 1
+@pytest.mark.parametrize("solution", solutions)
+def test_1(solution):
+    assert solution([1, 3, 5, 6], 5) == 2
 
 
-def test_3():
-    assert Solution().searchInsert([1, 3, 5, 6], 7) == 4
+@pytest.mark.parametrize("solution", solutions)
+def test_2(solution):
+    assert solution([1, 3, 5, 6], 2) == 1
+
+
+@pytest.mark.parametrize("solution", solutions)
+def test_3(solution):
+    assert solution([1, 3, 5, 6], 7) == 4
