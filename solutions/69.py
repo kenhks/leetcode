2d2699626682366482a1b654e20fd8da4baaae60
Ken Ho <kenhks014@gmail.com>
2022-08-25 13:49:11 +0800

    parametric solution 69

--- solutions/69.py
+++ solutions/69.py
@@ -1,10 +1,21 @@
+import pytest
+
+from utils import parametrize_solution_cls
+
+
 class Solution:
+    """
+    Binary search
+    Time Complexity: O(log(2, n))
+    Space Complexity: O(1)
+    """
+
     def mySqrt(self, x: int) -> int:
         l, r = 0, x
         while l < r:
             mid = (l + r) // 2
             mid_square = mid * mid
-            if mid_square <= x < mid_square + 2 * mid + 1:
+            if mid_square <= x < (mid + 1) * (mid + 1):
                 return mid
             elif mid_square < x:
                 l = mid + 1
@@ -13,29 +24,44 @@ class Solution:
         return r
 
 
-def test_1():
-    assert Solution().mySqrt(4) == 2
+solutions = parametrize_solution_cls(
+    [
+        Solution,
+    ],
+    "mySqrt",
+)
 
 
-def test_2():
-    assert Solution().mySqrt(8) == 2
+@pytest.mark.parametrize("solution", solutions)
+def test_1(solution):
+    assert solution(4) == 2
 
 
-def test_3():
-    assert Solution().mySqrt(0) == 0
+@pytest.mark.parametrize("solution", solutions)
+def test_2(solution):
+    assert solution(8) == 2
 
 
-def test_4():
-    assert Solution().mySqrt(1) == 1
+@pytest.mark.parametrize("solution", solutions)
+def test_3(solution):
+    assert solution(0) == 0
 
 
-def test_5():
-    assert Solution().mySqrt(2) == 1
+@pytest.mark.parametrize("solution", solutions)
+def test_4(solution):
+    assert solution(1) == 1
 
 
-def test_6():
-    assert Solution().mySqrt(80) == 8
+@pytest.mark.parametrize("solution", solutions)
+def test_5(solution):
+    assert solution(2) == 1
 
 
-def test_7():
-    assert Solution().mySqrt(12345) == 111
+@pytest.mark.parametrize("solution", solutions)
+def test_6(solution):
+    assert solution(80) == 8
+
+
+@pytest.mark.parametrize("solution", solutions)
+def test_7(solution):
+    assert solution(12345) == 111
