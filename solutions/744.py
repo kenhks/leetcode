493d272424ef290e8d8e4f3712159ca71a8bae8b
Ken Ho <kenhks014@gmail.com>
2022-08-26 18:26:53 +0800

    add solution 744

---
+++ solutions/744.py
@@ -0,0 +1,78 @@
+from typing import List
+
+import pytest
+
+from utils import parametrize_solution_cls
+
+
+class Solution:
+    """
+    Linear Scan
+    Time Complexity: O(n)
+    Space Complexity: O(1)
+    """
+
+    def nextGreatestLetter(self, letters: List[str], target: str) -> str:
+        for c in letters:
+            if c > target:
+                return c
+        return letters[0]
+
+
+class Solution2:
+    """
+    Binary search
+    Time Complexity: O(log(2, n))
+    Space Complexity: O(1)
+    """
+
+    def nextGreatestLetter(self, letters: List[str], target: str) -> str:
+        l, r = 0, len(letters) - 1
+        if target >= letters[-1] or target < letters[0]:
+            return letters[0]
+        while l <= r:
+            mid = (l + r) // 2
+            if letters[mid] <= target:
+                l = mid + 1
+            else:
+                r = mid - 1
+        return letters[l]
+
+
+solutions = parametrize_solution_cls(
+    [
+        Solution,
+        Solution2,
+    ],
+    "nextGreatestLetter",
+)
+
+
+@pytest.mark.parametrize("solution", solutions)
+def test_1(solution):
+    assert solution(["c", "f", "j"], "a") == "c"
+
+
+@pytest.mark.parametrize("solution", solutions)
+def test_2(solution):
+    assert solution(["c", "f", "j"], "c") == "f"
+
+
+@pytest.mark.parametrize("solution", solutions)
+def test_3(solution):
+    assert solution(["c", "f", "j"], "d") == "f"
+
+
+@pytest.mark.parametrize("solution", solutions)
+def test_4(solution):
+    assert solution(["c", "f", "j"], "x") == "c"
+
+
+@pytest.mark.parametrize("solution", solutions)
+def test_5(solution):
+    assert solution(["a", "b", "c", "d", "e", "f", "g"], "d") == "e"
+
+
+@pytest.mark.parametrize("solution", solutions)
+def test_6(solution):
+    assert solution(["a", "b", "c"], "z") == "a"
