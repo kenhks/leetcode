84ca3386d981d0f95580924b07dd8689629c81a7
Ken Ho <kenhks014@gmail.com>
2022-08-27 15:40:43 +0800

    parametric solution 167

--- solutions/167.py
+++ solutions/167.py
@@ -1,20 +1,37 @@
 from typing import List
 
+import pytest
+
+from utils import parametrize_solution_cls
+
 
 class Solution:
+    """
+    Two pointer
+    Time Complexity: O(n)
+    Space Complexity: O(1)
+    """
+
     def twoSum(self, numbers: List[int], target: int) -> List[int]:
-        l, r = 0, len(numbers) - 1
-        while l < r:
-            c_target = numbers[l] + numbers[r]
+        left, right = 0, len(numbers) - 1
+        while left < right:
+            c_target = numbers[left] + numbers[right]
             if c_target < target:
-                l += 1
+                left += 1
             elif c_target > target:
-                r -= 1
+                right -= 1
             else:
-                return [l + 1, r + 1]
+                break
+        return [left + 1, right + 1]
 
 
 class Solution2:
+    """
+    Scan with hashmap
+    Time Complexity: O(n)
+    Space Complexity: O(n)
+    """
+
     def twoSum(self, numbers: List[int], target: int) -> List[int]:
         h = {}
         for i, num in enumerate(numbers, start=1):
@@ -22,27 +39,29 @@ class Solution2:
             if n not in h:
                 h[num] = i
             else:
-                return [h[n], i]
-
-def test_1():
-    assert Solution().twoSum([2, 7, 11, 15], 9) == [1, 2]
+                break
+        return [h[n], i]
 
 
-def test_2():
-    assert Solution().twoSum([2, 3, 4], 6) == [1, 3]
+solutions = parametrize_solution_cls(
+    [
+        Solution,
+        Solution2,
+    ],
+    "twoSum",
+)
 
 
-def test_3():
-    assert Solution().twoSum([-1, 0], -1) == [1, 2]
+@pytest.mark.parametrize("solution", solutions)
+def test_1(solution):
+    assert solution([2, 7, 11, 15], 9) == [1, 2]
 
 
-def test_4():
-    assert Solution2().twoSum([2, 7, 11, 15], 9) == [1, 2]
+@pytest.mark.parametrize("solution", solutions)
+def test_2(solution):
+    assert solution([2, 3, 4], 6) == [1, 3]
 
 
-def test_5():
-    assert Solution2().twoSum([2, 3, 4], 6) == [1, 3]
-
-
-def test_6():
-    assert Solution2().twoSum([-1, 0], -1) == [1, 2]
+@pytest.mark.parametrize("solution", solutions)
+def test_3(solution):
+    assert solution([-1, 0], -1) == [1, 2]
