0f321c101aff181e842d6f7fdd151407589ee526
Ken Ho <kenhks014@gmail.com>
2022-08-27 15:52:11 +0800

    parametric solution 566

--- solutions/566.py
+++ solutions/566.py
@@ -1,7 +1,17 @@
 from typing import List
 
+import pytest
+
+from utils import parametrize_solution_cls
+
 
 class Solution:
+    """
+    Append element one by one
+    Time Complexity: O(mn)
+    Space Complexity: O(mn)
+    """
+
     def matrixReshape(self, mat: List[List[int]], r: int, c: int) -> List[List[int]]:
         m, n = len(mat), len(mat[0])
         if r * c != m * n:
@@ -11,7 +21,6 @@ class Solution:
         for i in range(m):
             for j in range(n):
                 if len(row) < c:
-                    print("append")
                     row.append(mat[i][j])
                 if len(row) == c:
                     new_mat.append(row)
@@ -19,17 +28,27 @@ class Solution:
         return new_mat
 
 
-def test_1():
+solutions = parametrize_solution_cls(
+    [
+        Solution,
+    ],
+    "matrixReshape",
+)
+
+
+@pytest.mark.parametrize("solution", solutions)
+def test_1(solution):
     mat = [
         [1, 2],
         [3, 4],
     ]
-    assert Solution().matrixReshape(mat, r=1, c=4) == [[1, 2, 3, 4]]
+    assert solution(mat, r=1, c=4) == [[1, 2, 3, 4]]
 
 
-def test_2():
+@pytest.mark.parametrize("solution", solutions)
+def test_2(solution):
     mat = [
         [1, 2],
         [3, 4],
     ]
-    assert Solution().matrixReshape(mat, r=2, c=4) == [[1, 2], [3, 4]]
+    assert solution(mat, r=2, c=4) == [[1, 2], [3, 4]]
