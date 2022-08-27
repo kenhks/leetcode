684a3b73e0975a30b1ce6362e548f56b2a453e01
Ken Ho <kenhks014@gmail.com>
2022-08-27 20:08:15 +0800

    add solution 345

---
+++ solutions/345.py
@@ -0,0 +1,50 @@
+import pytest
+
+from utils import parametrize_solution_cls
+
+
+class Solution:
+    """
+    Python built-it lower()
+    Time Complexity: O(n) = n
+    Space Complexity: O(n) = n
+    """
+
+    vowels = set(["a", "e", "i", "o", "u", "A", "E", "I", "O", "U"])
+
+    def reverseVowels(self, s: str) -> str:
+        s = list(s)
+        left, right = 0, len(s) - 1
+        while left < right:
+            if s[left] not in self.vowels:
+                left += 1
+            if s[right] not in self.vowels:
+                right -= 1
+            if s[left] in self.vowels and s[right] in self.vowels:
+                s[left], s[right] = s[right], s[left]
+                left += 1
+                right -= 1
+        return "".join(s)
+
+
+solutions = parametrize_solution_cls(
+    [
+        Solution,
+    ],
+    "reverseVowels",
+)
+
+
+@pytest.mark.parametrize("solution", solutions)
+def test_1(solution):
+    assert solution("hello") == "holle"
+
+
+@pytest.mark.parametrize("solution", solutions)
+def test_2(solution):
+    assert solution("leetcode") == "leotcede"
+
+
+@pytest.mark.parametrize("solution", solutions)
+def test_3(solution):
+    assert solution("abc") == "abc"
