7e42a4a3e9b05eb7cf11100444b30b927a4d41cf
Ken Ho <kenhks014@gmail.com>
2022-08-27 15:08:51 +0800

    parametric solution 1822

--- solutions/1822.py
+++ solutions/1822.py
@@ -1,7 +1,17 @@
 from typing import List
 
+import pytest
+
+from utils import parametrize_solution_cls
+
 
 class Solution:
+    """
+    Linear Scan
+    Time Complexity: O(n)
+    Space Complexity: O(1)
+    """
+
     def arraySign(self, nums: List[int]) -> int:
         sign = 1
         for i in nums:
@@ -13,13 +23,24 @@ class Solution:
         return sign
 
 
-def test_1():
-    assert Solution().arraySign([-1, -2, -3, -4, 3, 2, 1]) == 1
+solutions = parametrize_solution_cls(
+    [
+        Solution,
+    ],
+    "arraySign",
+)
 
 
-def test_2():
-    assert Solution().arraySign([1, 5, 0, 2, -3]) == 0
+@pytest.mark.parametrize("solution", solutions)
+def test_1(solution):
+    assert solution([-1, -2, -3, -4, 3, 2, 1]) == 1
 
 
-def test_3():
-    assert Solution().arraySign([-1, 1, -1, 1, -1]) == -1
+@pytest.mark.parametrize("solution", solutions)
+def test_2(solution):
+    assert solution([1, 5, 0, 2, -3]) == 0
+
+
+@pytest.mark.parametrize("solution", solutions)
+def test_3(solution):
+    assert solution([-1, 1, -1, 1, -1]) == -1
