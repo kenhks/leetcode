6b5b5eb944e9bc81c445191b36d2158fa973e29e
Ken Ho <kenhks014@gmail.com>
2022-08-27 15:30:36 +0800

    parametric solution 1491

--- solutions/1491.py
+++ solutions/1491.py
@@ -1,9 +1,19 @@
 from typing import List
 
+import pytest
+
+from utils import parametrize_solution_cls
+
 
 class Solution:
+    """
+    Scan
+    Time Complexity: O(n)
+    Space Complexity: O(1)
+    """
+
     def average(self, salary: List[int]) -> float:
-        min_salary, max_salary = 10 ** 6, 1000
+        min_salary, max_salary = 1_000_000, 1000
         total = 0
         for i in salary:
             if i < min_salary:
@@ -14,12 +24,24 @@ class Solution:
         return (total - min_salary - max_salary) / (len(salary) - 2)
 
 
-def test_1():
-    assert Solution().average([4000, 3000, 1000, 2000]) == 2500
+solutions = parametrize_solution_cls(
+    [
+        Solution,
+    ],
+    "average",
+)
 
 
-def test_2():
-    assert Solution().average([1000, 2000, 3000]) == 2000
+@pytest.mark.parametrize("solution", solutions)
+def test_1(solution):
+    assert solution([4000, 3000, 1000, 2000]) == 2500
 
-def test_3():
-    assert Solution().average([1000, 1000, 3000]) == 1000
+
+@pytest.mark.parametrize("solution", solutions)
+def test_2(solution):
+    assert solution([1000, 2000, 3000]) == 2000
+
+
+@pytest.mark.parametrize("solution", solutions)
+def test_3(solution):
+    assert solution([1000, 1000, 3000]) == 1000
