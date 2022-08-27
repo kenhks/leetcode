33f6172c0bc22856c2bc980afd661661988adfe8
Ken Ho <kenhks014@gmail.com>
2022-08-27 15:20:44 +0800

    parametric solution 1232

--- solutions/1232.py
+++ solutions/1232.py
@@ -1,31 +1,73 @@
 from typing import List
 
+import pytest
+
+from utils import parametrize_solution_cls
+
 
 class Solution:
+    """
+    Scan
+    Time Complexity: O(n)
+    Space Complexity: O(1)
+    """
+
     def checkStraightLine(self, coordinates: List[List[int]]) -> bool:
         x0, y0 = coordinates[0]
         x1, y1 = coordinates[1]
         dy, dx = (y1 - y0), (x1 - x0)
-        return all(dy * (x - x0) == dx * (y - y0) for x, y in coordinates)
+        return not any(dy * (x - x0) != dx * (y - y0) for x, y in coordinates)
 
 
-def test_1():
-    assert (
-        Solution().checkStraightLine([[1, 2], [2, 3], [3, 4], [4, 5], [5, 6], [6, 7]])
-        == True
-    )
+solutions = parametrize_solution_cls(
+    [
+        Solution,
+    ],
+    "checkStraightLine",
+)
 
 
-def test_2():
-    assert (
-        Solution().checkStraightLine([[1, 1], [2, 2], [3, 4], [4, 5], [5, 6], [7, 7]])
-        == False
-    )
+@pytest.mark.parametrize("solution", solutions)
+def test_1(solution):
+    coordinates = [
+        [1, 2],
+        [2, 3],
+        [3, 4],
+        [4, 5],
+        [5, 6],
+        [6, 7],
+    ]
+    assert solution(coordinates)
 
 
-def test_3():
-    assert Solution().checkStraightLine([[0, 0], [0, 1], [0, -1]]) == True
+@pytest.mark.parametrize("solution", solutions)
+def test_2(solution):
+    coordinates = [
+        [1, 1],
+        [2, 2],
+        [3, 4],
+        [4, 5],
+        [5, 6],
+        [7, 7],
+    ]
+    assert not solution(coordinates)
 
 
-def test_4():
-    assert Solution().checkStraightLine([[0, 0], [0, 1], [1, 0]]) == False
+@pytest.mark.parametrize("solution", solutions)
+def test_3(solution):
+    coordinates = [
+        [0, 0],
+        [0, 1],
+        [0, -1],
+    ]
+    assert solution(coordinates)
+
+
+@pytest.mark.parametrize("solution", solutions)
+def test_4(solution):
+    coordinates = [
+        [0, 0],
+        [0, 1],
+        [1, 0],
+    ]
+    assert not solution(coordinates)
