fd2a7470d45e2f167a288e0048e4ca872c2ebc26
Ken Ho <kenhks014@gmail.com>
2022-08-27 15:47:26 +0800

    parametric solution 283

--- solutions/283.py
+++ solutions/283.py
@@ -1,7 +1,17 @@
 from typing import List
 
+import pytest
+
+from utils import parametrize_solution_cls
+
 
 class Solution:
+    """
+    Pop and append
+    Time Complexity: O(n)
+    Space Complexity: O(n)
+    """
+
     def moveZeroes(self, nums: List[int]) -> None:
         """
         Do not return anything, modify nums in-place instead.
@@ -17,6 +27,12 @@ class Solution:
 
 
 class Solution2:
+    """
+    Swap in place
+    Time Complexity: O(n)
+    Space Complexity: O(1)
+    """
+
     def moveZeroes(self, nums: List[int]) -> None:
         """
         Do not return anything, modify nums in-place instead.
@@ -28,13 +44,24 @@ class Solution2:
                 next_nonzero += 1
 
 
-def test_1():
-    l = [0, 1, 0, 3, 12]
-    Solution().moveZeroes(l)
-    assert l == [1, 3, 12, 0, 0]
+solutions = parametrize_solution_cls(
+    [
+        Solution,
+        Solution2,
+    ],
+    "moveZeroes",
+)
 
 
-def test_2():
-    l = [0, 1, 0, 3, 12]
-    Solution2().moveZeroes(l)
-    assert l == [1, 3, 12, 0, 0]
+@pytest.mark.parametrize("solution", solutions)
+def test_1(solution):
+    nums = [0, 1, 0, 3, 12]
+    solution(nums)
+    assert nums == [1, 3, 12, 0, 0]
+
+
+@pytest.mark.parametrize("solution", solutions)
+def test_2(solution):
+    nums = [0, 1, 0, 3, 12]
+    Solution2().moveZeroes(nums)
+    assert nums == [1, 3, 12, 0, 0]
