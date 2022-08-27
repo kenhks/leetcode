242ba9d1fcf2d4b79820899b12e4d733552ae1b9
Ken Ho <kenhks014@gmail.com>
2022-08-27 15:13:42 +0800

    parametric solution 1502

--- solutions/1502.py
+++ solutions/1502.py
@@ -1,7 +1,17 @@
 from typing import List
 
+import pytest
+
+from utils import parametrize_solution_cls
+
 
 class Solution:
+    """
+    Sort then scan
+    Time Complexity: O(nlog(2, n))
+    Space Complexity: O(1)
+    """
+
     def canMakeArithmeticProgression(self, arr: List[int]) -> bool:
         if len(arr) == 2:
             return True
@@ -14,9 +24,19 @@ class Solution:
             return True
 
 
-def test_1():
-    assert Solution().canMakeArithmeticProgression([3, 5, 1]) == True
+solutions = parametrize_solution_cls(
+    [
+        Solution,
+    ],
+    "canMakeArithmeticProgression",
+)
 
 
-def test_2():
-    assert Solution().canMakeArithmeticProgression([1, 2, 4]) == False
+@pytest.mark.parametrize("solution", solutions)
+def test_1(solution):
+    assert solution([3, 5, 1])
+
+
+@pytest.mark.parametrize("solution", solutions)
+def test_2(solution):
+    assert not solution([1, 2, 4])
