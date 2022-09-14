21c0fcc10977a6aa7ec7e6a4d9fa180da488cbad
Ken Ho <kenhks014@gmail.com>
2022-09-14 13:35:20 +0800

    add solution 1464

---
+++ solutions/1464.py
@@ -0,0 +1,40 @@
+from typing import List
+
+import pytest
+
+from utils import parametrize_solution_cls
+
+
+class Solution:
+    """
+    Sort desecnding
+    Time Complexity: O(nlog(2, n))
+    Space Complexity: O(1)
+    """
+
+    def maxProduct(self, nums: List[int]) -> int:
+        nums.sort(reverse=True)
+        return (nums[0] - 1) * (nums[1] - 1)
+
+
+solutions = parametrize_solution_cls(
+    [
+        Solution,
+    ],
+    "maxProduct",
+)
+
+
+@pytest.mark.parametrize("solution", solutions)
+def test_1(solution):
+    assert solution([3, 4, 5, 2]) == 12
+
+
+@pytest.mark.parametrize("solution", solutions)
+def test_2(solution):
+    assert solution([1, 5, 4, 5]) == 16
+
+
+@pytest.mark.parametrize("solution", solutions)
+def test_3(solution):
+    assert solution([3, 7]) == 12
