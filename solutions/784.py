955d19253c58c9f8123a5a42757069fa55a02cf3
Ken Ho <kenhks014@gmail.com>
2022-09-27 18:31:00 +0800

    parametric solution 784

--- solutions/784.py
+++ solutions/784.py
@@ -1,7 +1,17 @@
 from typing import List
 
+import pytest
+
+from utils import parametrize_solution_cls
+
 
 class Solution:
+    """
+    Time Complexity: O(n)
+    Space Complexity: O(2^n)
+    n = len(s)
+    """
+
     def letterCasePermutation(self, s: str) -> List[str]:
         def possible_chars(char):
             if char.isdigit():
@@ -9,32 +19,39 @@ class Solution:
             elif char.isalpha():
                 return [char.lower(), char.upper()]
 
-        result = [[]]
+        result: List[List[str]] = [[]]
         for char in s:
             chars = possible_chars(char)
             if len(chars) == 1:
                 result = [i + chars for i in result]
             elif len(chars) == 2:
-                result = [i + chars[:1] for i in result] + [
-                    i + chars[1:] for i in result
-                ]
-        result = ["".join(i) for i in result]
-        return result
+                result = [i + chars[:1] for i in result] + [i + chars[1:] for i in result]
+        return ["".join(i) for i in result]
 
 
-def test_1():
+solutions = parametrize_solution_cls(
+    [
+        Solution,
+    ],
+    "letterCasePermutation",
+)
+
+
+@pytest.mark.parametrize("solution", solutions)
+def test_1(solution):
     expected = [
         "a1b2",
         "a1B2",
         "A1b2",
         "A1B2",
     ]
-    assert sorted(Solution().letterCasePermutation("a1b2")) == sorted(expected)
+    assert sorted(solution("a1b2")) == sorted(expected)
 
 
-def test_2():
+@pytest.mark.parametrize("solution", solutions)
+def test_2(solution):
     expected = [
         "3z4",
         "3Z4",
     ]
-    assert sorted(Solution().letterCasePermutation("3z4")) == sorted(expected)
+    assert sorted(solution("3z4")) == sorted(expected)
