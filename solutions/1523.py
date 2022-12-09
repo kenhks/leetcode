7bfb4fd6e0f7767a32bf346c5b2e10a4bc72fd69
Ken Ho <kenhks014@gmail.com>
2022-12-09 18:16:42 +0800

    parametric solution 1523

--- solutions/1523.py
+++ solutions/1523.py
@@ -1,4 +1,15 @@
+import pytest
+
+from utils import parametrize_solution_cls
+
+
 class Solution:
+    """
+    Math
+    Time Complexity: O(1)
+    Space Complexity: O(1)
+    """
+
     def countOdds(self, low: int, high: int) -> int:
         offset = 0
         if low % 2 == 1:
@@ -8,25 +19,39 @@ class Solution:
         return offset + (high - low) // 2
 
 
-def test_1():
-    assert Solution().countOdds(3, 7) == 3
+solutions = parametrize_solution_cls(
+    [
+        Solution,
+    ],
+    "countOdds",
+)
 
 
-def test_2():
-    assert Solution().countOdds(8, 10) == 1
+@pytest.mark.parametrize("solution", solutions)
+def test_1(solution):
+    assert solution(3, 7) == 3
 
 
-def test_3():
-    assert Solution().countOdds(8, 8) == 0
+@pytest.mark.parametrize("solution", solutions)
+def test_2(solution):
+    assert solution(8, 10) == 1
 
 
-def test_4():
-    assert Solution().countOdds(1, 1) == 1
+@pytest.mark.parametrize("solution", solutions)
+def test_3(solution):
+    assert solution(8, 8) == 0
 
 
-def test_5():
-    assert Solution().countOdds(1, 10) == 5
+@pytest.mark.parametrize("solution", solutions)
+def test_4(solution):
+    assert solution(1, 1) == 1
 
 
-def test_6():
-    assert Solution().countOdds(10, 15) == 3
+@pytest.mark.parametrize("solution", solutions)
+def test_5(solution):
+    assert solution(1, 10) == 5
+
+
+@pytest.mark.parametrize("solution", solutions)
+def test_6(solution):
+    assert solution(10, 15) == 3
