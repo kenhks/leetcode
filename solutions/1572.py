f4e65385eda74be0846189814560a958d154fd6f
Ken Ho <kenhks014@gmail.com>
2022-12-09 18:16:41 +0800

    parametric solution 1572

--- solutions/1572.py
+++ solutions/1572.py
@@ -1,29 +1,49 @@
 from typing import List
 
+import pytest
+
+from utils import parametrize_solution_cls
+
 
 class Solution:
+    """
+    Two pointer
+    Time Complexity: O(n)
+    Space Complexity: O(1)
+    """
+
     def diagonalSum(self, mat: List[List[int]]) -> int:
         n = len(mat)
-        mat_sum = 0 if n % 2 == 0 else -mat[n // 2][n // 2]
+        ans = 0 if n % 2 == 0 else -mat[n // 2][n // 2]
         for y in range(n):
-            mat_sum += mat[y][y] + mat[y][n - 1 - y]
-        return mat_sum
+            ans += mat[y][y] + mat[y][n - 1 - y]
+        return ans
 
 
-def test_1():
+solutions = parametrize_solution_cls(
+    [
+        Solution,
+    ],
+    "diagonalSum",
+)
+
+
+@pytest.mark.parametrize("solution", solutions)
+def test_1(solution):
     mat = [
         [1, 2, 3],
         [4, 5, 6],
         [7, 8, 9],
     ]
-    assert Solution().diagonalSum(mat) == 25
+    assert solution(mat) == 25
 
 
-def test_2():
+@pytest.mark.parametrize("solution", solutions)
+def test_2(solution):
     mat = [
         [1, 1, 1, 1],
         [1, 1, 1, 1],
         [1, 1, 1, 1],
         [1, 1, 1, 1],
     ]
-    assert Solution().diagonalSum(mat) == 8
+    assert solution(mat) == 8
