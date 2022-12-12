3ced11b5703437ec10d61c7d3e93a27cd3b60007
Ken Ho <kenhks014@gmail.com>
2022-12-12 18:45:02 +0800

    add solution 674

---
+++ solutions/674.py
@@ -0,0 +1,46 @@
+from typing import List
+import pytest
+
+from utils import parametrize_solution_cls
+
+
+class Solution:
+    """
+    Time Complexity: O(n)
+    Space Complexity: O(1)
+    """
+
+    def findLengthOfLCIS(self, nums: List[int]) -> int:
+        c_len, max_len = 1, 1
+        prev_n = nums[0]
+        for n in nums[1:]:
+            if n > prev_n:
+                c_len += 1
+                max_len = max(c_len, max_len)
+            else:
+                c_len = 1
+            prev_n = n
+        return max_len
+
+
+solutions = parametrize_solution_cls(
+    [
+        Solution,
+    ],
+    "findLengthOfLCIS",
+)
+
+
+@pytest.mark.parametrize("solution", solutions)
+def test_1(solution):
+    assert solution(nums=[1, 3, 5, 4, 7]) == 3
+
+
+@pytest.mark.parametrize("solution", solutions)
+def test_2(solution):
+    assert solution(nums=[2, 2, 2, 2, 2]) == 1
+
+
+@pytest.mark.parametrize("solution", solutions)
+def test_3(solution):
+    assert solution(nums=[1]) == 1
