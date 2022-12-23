75168ce987b0eda44a3831580dea72fb2751ed1d
Ken Ho <kenhks014@gmail.com>
2022-12-23 23:43:07 +0800

    improve solution 617

--- solutions/617.py
+++ solutions/617.py
@@ -1,3 +1,4 @@
+from collections import deque
 from typing import Optional
 
 import pytest
@@ -8,7 +9,7 @@ from utils import create_Tree, get_Tree_values, parametrize_solution_cls
 
 class Solution:
     """
-    DFS
+    DFS, Recursive
     Time Complexity: O(n)
     Space Complexity: O(h)
     h = height of tree ~= log(2, n)
@@ -24,9 +25,39 @@ class Solution:
             return root1 or root2
 
 
+class Solution2:
+    """
+    DFS, Recursive
+    Time Complexity: O(n)
+    Space Complexity: O(h)
+    h = height of tree ~= log(2, n)
+    """
+
+    def mergeTrees(self, root1: Optional[TreeNode], root2: Optional[TreeNode]) -> Optional[TreeNode]:
+        if root1 and root2:
+            root1.val += root2.val
+            stack = deque([(root1, root2)])
+            while stack:
+                for _ in range(len(stack)):
+                    node1, node2 = stack.popleft()
+                    if node1 or node2:
+                        if node1.left and node2.left:
+                            stack.append((node1.left, node2.left))
+                            node1.left.val += node2.left.val
+                        elif node2.left:
+                            node1.left = node2.left
+                        if node1.right and node2.right:
+                            stack.append((node1.right, node2.right))
+                            node1.right.val += node2.right.val
+                        elif node2.right:
+                            node1.right = node2.right
+        return root1 or root2
+
+
 solutions = parametrize_solution_cls(
     [
         Solution,
+        Solution2,
     ],
     "mergeTrees",
 )
@@ -43,4 +74,11 @@ def test_1(solution):
 def test_2(solution):
     root1 = create_Tree([1])
     root2 = create_Tree([1, 2])
-    assert get_Tree_values(solution(root1, root2)) == [2, 2, None]
+    assert get_Tree_values(solution(root1, root2)) == [2, 2]
+
+
+@pytest.mark.parametrize("solution", solutions)
+def test_3(solution):
+    root1 = create_Tree([])
+    root2 = create_Tree([1])
+    assert get_Tree_values(solution(root1, root2)) == [1]
