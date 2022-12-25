69b410634f33d6caa14870ef972e30e3bf3fc623
Ken Ho <kenhks014@gmail.com>
2022-12-26 02:57:07 +0800

    add solution 455

---
+++ solutions/455.py
@@ -0,0 +1,44 @@
+from typing import List
+
+import pytest
+
+from utils import parametrize_solution_cls
+
+
+class Solution:
+    """
+    Two Pointer with sort
+    Time Complexity: O(mlog(2, m) + nlog(2, n)) = mlog(2, m) + nlog(2, n) + m + n
+    Space Complexity: O(1)
+    """
+
+    def findContentChildren(self, g: List[int], s: List[int]) -> int:
+        g.sort()
+        s.sort()
+        ans = 0
+        i, j = 0, 0
+        while i < len(g) and j < len(s):
+            if s[j] >= g[i]:
+                ans += 1
+                i += 1
+            j += 1
+        return ans
+
+
+solutions = parametrize_solution_cls(
+    [
+        Solution,
+    ],
+    "findContentChildren",
+)
+
+
+@pytest.mark.parametrize("solution", solutions)
+def test_1(solution):
+
+    assert solution(g=[1, 2, 3], s=[1, 1]) == 1
+
+
+@pytest.mark.parametrize("solution", solutions)
+def test_2(solution):
+    assert solution(g=[1, 2], s=[1, 2, 3]) == 2
