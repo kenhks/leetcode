e4cf2f9ba16ace4f1b07a27667bbb5400d38dc3d
Ken Ho <kenhks014@gmail.com>
2023-01-01 07:49:06 +0800

    add solution 561

---
+++ solutions/561.py
@@ -0,0 +1,35 @@
+from typing import List
+
+import pytest
+
+from utils import parametrize_solution_cls
+
+
+class Solution:
+    """
+    Sort
+    Time Complexity: O(nlog(2, n)) = nlog(2, n) + n
+    Space Complexity: O(1)
+    """
+
+    def arrayPairSum(self, nums: List[int]) -> int:
+        nums.sort()
+        return sum(nums[::2])
+
+
+solutions = parametrize_solution_cls(
+    [
+        Solution,
+    ],
+    "arrayPairSum",
+)
+
+
+@pytest.mark.parametrize("solution", solutions)
+def test_1(solution):
+    assert solution(nums=[1, 4, 3, 2]) == 4
+
+
+@pytest.mark.parametrize("solution", solutions)
+def test_2(solution):
+    assert solution(nums=[6, 2, 6, 5, 1, 2]) == 9
